fbc132dec521a39fa83f783adb4dced53f086430
Collin Morudi <realcollin112@gmail.com>
2019-12-05 14:43:00 +0200

    added my solutions to the exercises

---
+++ Solutions/mySolutions.js
@@ -0,0 +1,11 @@
+// Write a function identity that takes an argument and returns that argument
+const identity = (x) => x;
+console.log(identity('Hello there!'));
+
+// Write a binary function addb that takes two numbers and returns their sum
+const addb = (x, y) => x + y;
+console.log(addb(3, 7));
+
+// Write a binary function mulb that takes two numbers and returns their product
+const mulb = (x, y) => x * y;
+console.log(mulb(2, 5));

177be4042c1b64b4b102ff860afa66d0d3586ee3
shotaro kaneda <juanpablolvl99@gmail.com>
2020-05-04 14:44:12 -0300

    Adding from twiceUnary to filter.

--- Solutions/my_solution_KN.js
+++ Solutions/my_solution_KN.js
@@ -191,4 +191,127 @@ console.log('inc => ' + inc(inc(5)));
 // Write a function twiceUnary that takes a binary function and returns a unary function that passes 
 // its argument to the binary function twice
 let twiceUnary = (binary) => (x) => binary(x, x);
-console.log('twiceUnary => ' + twiceUnary(mulb)(12));
+console.log('twiceUnary => ' + twiceUnary(subb)(12));
+
+// Use the function twiceUnary to create the doubl function
+const doubl = twiceUnary(addb);
+console.log('doubl => ' + doubl(12));
+
+// Use the function twiceUnary to create the square function
+const square = twiceUnary(mulb);
+console.log('square => ' + square(12));
+
+// Write a function twice that is generalized for any amount of arguments
+const twice = (func) => (...args) => func(...args, ...args);
+console.log('twice => ' + twice(mul)(1, 2, 4));
+
+// Write a function reverseb that reverses the arguments of a binary function
+const reverseb = (binary) => (x, y) => binary(y, x);
+console.log('reverseb => ' + reverseb(subb)(1, 2));
+
+// Write a function reverse that is generalized for any amount of arguments
+const reverse = (func) => (...args) => func(...args.reverse());
+console.log('reverse => ' + reverse(sub)(1, 2, 4));
+
+// Write a function composeuTwo that takes two unary functions and returns a unary function that 
+// calls them both
+const composeuTwo = (unary1, unary2) => (x) => unary2(unary1(x));
+console.log('composeuTwo => ' + composeuTwo(doubl, square)(5));
+
+// Write a function composeu that is generalized for any amount of arguments
+const composeu = (...funcs) => (x) => {
+    let partial = x;
+    funcs.forEach(func => {
+        partial = func(partial);
+    })
+    return partial;
+}
+console.log('composeu => ' + composeu(doubl, square, identity, curry(add, 1, 2))(5));
+
+// Write a function composeb that takes two binary functions and returns a function that calls them both
+const composeb = (binary1, binary2) => (a, b, c) => binary2(binary1(a, b), c);
+console.log('composeb => ' + composeb(addb, mulb)(2, 3, 7));
+
+// Write a function composeTwo that takes two functions and returns a function that calls them both
+const composeTwo = (func1, func2) => (...args) => func2(func1(...args));
+console.log('composeTwo => ' + composeTwo(add, square)(2, 3, 7));
+
+// Write a function compose that takes any amount of functions and returns a function that takes any amount 
+// of arguments and gives them to the first function, then that result to the second function and so on
+const compose = (...funcs) => (...args) => {
+    let partial = args;
+    funcs.forEach(func => {
+        partial = Array.isArray(partial) ? func(...partial) : func(partial);
+    });
+    return partial;
+}
+console.log('compose => ' + compose(add, doubl, fill, max)(0, 1, 2));
+
+// Write a function limitb that allows a binary function to be called a limited number of times
+const limitb = (binary, lmt) => (x, y) => lmt-- <= 0 ? undefined : binary(x, y);
+let addLmtb = limitb(addb, 1);
+console.log('limitb =>', addLmtb(3, 4), addLmtb(3, 5));
+
+// Write a function limit that is generalized for any amount of arguments
+const limit = (func, lmt) => (...args) => lmt-- <= 0 ? undefined : func(...args);
+let addLmt = limit(add, 1);
+console.log('limit =>', addLmt(1, 2, 4), addLmt(3, 5, 9, 2));
+
+// Write a function genFrom that produces a generator that will produces a series of values
+const genFrom = (v) => () => v++;
+let index = genFrom(1);
+console.log('genFrom =>', index(), index(), index());
+
+// Write a function genTo that takes a generator and an end limit, and returns a generator that 
+// will produce numbers up to that limit
+const genTo = (gen, lmt) => () => {
+    let val = gen();
+    return lmt > val ? val : undefined;
+}
+index = genTo(genFrom(1), 3);
+console.log('genTo =>', index(), index(), index());
+
+// Write a function genFromTo that produces a generator that will produce values in a range
+const genFromTo = (start, end) => () => start < end ? start++ : undefined;
+index = genFromTo(0, 3);
+console.log('genFromTo =>', index(), index(), index(), index());
+
+// Write a function elementGen that takes an array and a generator and returns a generator that will 
+// produce elements from the array
+const elementGen = (array, gen) => () => array[gen()];
+let el = elementGen(['a', 'b', 'c', 'd',], genFromTo(1, 3));
+console.log('elementGen =>', el(), el(), el());
+
+// Write a function element that is a modified elementGen function so that the generator argument is 
+// optional. If a generator is not provided, then each of the elements of the array will be produced.
+const element = (array) => {
+    let index = 0;
+    return () => array[index++];
+}
+el = element([-5, -4,]);
+console.log('element =>', el(), el(), el());
+
+// Write a function collect that takes a generator and an array and produces a function that will collect 
+// the results in the array.
+const collect = (gen, array) => () => {
+    let val = gen();
+    if (val !== undefined) {
+        array.push(val);
+    }
+    return val;
+}
+let array = [];
+let col = collect(genFromTo(0, 2), array);
+console.log('collect =>', col(), col(), col(), array);
+
+// Write a function filter that takes a generator and a predicate and produces a generator that produces only 
+// the values approved by the predicate
+const filter = (gen, predicate) => () => {
+    let val = gen();
+    if (val === undefined) {
+        return undefined;
+    }
+    return predicate(val) ? val : filter(gen, predicate)();
+}
+let fil = filter(genFromTo(0, 5), val => val % 3 === 0);
+console.log('filter => ', fil(), fil(), fil())

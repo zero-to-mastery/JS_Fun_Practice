92eebb27fae9c507f256ca89316b560e57de895a
unknown <d3kelley@gmail.com>
2020-07-19 15:03:49 -0400

    First commit to solution

---
+++ Solutions/d3kelleyy.js
@@ -0,0 +1,25 @@
+// Take and return argument
+const identity = (arg) => arg;
+
+identity(10); //Returns 10
+
+//Takes two numbers and returns sum
+
+const addb = (num1, num2) => num1 + num2;
+
+addb(5, 9); //Returns 14
+
+//Takes two numbers and returns difference
+const subb = (num1, num2) => num1 - num2;
+
+subb(20, 7); //Returns 13
+
+//Takes two numbers and returns product
+const mulb = (num1, num2) => num1 * num2;
+
+mulb(10, 12); //Returns 120
+
+//Takes two numbers and returns the smaller one
+const minb = (num1, num2) => Math.min(num1, num2);
+
+minb(70, 30); //Returns 30

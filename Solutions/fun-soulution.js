2a068c59afaf200d5d09dacf3eded85ada0eea1c
ZISAN <zisan13407@gmail.com>
2020-07-25 15:23:08 +0600

    Some more new function

--- Solutions/fun-soulution.js
+++ Solutions/fun-soulution.js
@@ -1,18 +1,18 @@
-//Write a function identity that takes an argument and returns that argument
+//1.Write a function identity that takes an argument and returns that argument
 	const identity = (x) => { return x };
 	identity(5); //returns 5 
 
-//Write a binary function addb that takes two numbers and returns their sum	
+//2.Write a binary function addb that takes two numbers and returns their sum	
 	const addb = (x,y) =>{ return x + y };
 	addb(5,6); //return 11
-//Write a binary function subb that takes two numbers and returns their difference	
+//3.Write a binary function subb that takes two numbers and returns their difference	
 	const subb = (x,y) =>{ return x - y };
 	subb(11,3) //return 8
 
-//Write a binary function mulb that takes two numbers and returns their product
+//3.Write a binary function mulb that takes two numbers and returns their product
 	const mulb = (x,y) =>{ return x * y };
 
-//Write a binary function minb that takes two numbers and returns the smaller one
+//4.Write a binary function minb that takes two numbers and returns the smaller one
 	const minb = (x,y) =>{ 
 		if (x>y) {
 			return y;
@@ -21,7 +21,7 @@
 		}
 	};
 
-//Write a binary function maxb that takes two numbers and returns the larger one
+//5.Write a binary function maxb that takes two numbers and returns the larger one
 	const maxb = (x,y) =>{ 
 		if (x > y) {
 			return x;
@@ -29,3 +29,19 @@
 			return y;
 		}
 	};
+//7.Write a function add that is generalized for any amount of arguments
+	const add = (...nums) =>{
+		return nums.reduce((a,b)=> a+b);
+	}
+	add(2,4,2,1,4) //return 13
+
+//8.Write a function sub that is generalized for any amount of arguments
+	const sub = (...nums) =>{
+		return nums.reduce((a,b)=> a-b);
+	}
+	sub(234,41,2,1,4) //return 186
+
+
+
+
+

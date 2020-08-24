7d1375519159c7f4111d94302a294b7c2bda3cc9
Mayank Aggarwal <mayank2aggarwal@gmail.com>
2020-08-25 00:04:02 +0530

    feat: solve some more questions

--- Solutions/mayank0255_solutions.js
+++ Solutions/mayank0255_solutions.js
@@ -10,11 +10,38 @@ const minb = (a, b) => a < b ? a : b;
 
 const maxb = (a, b) => a > b ? a : b;
 
+const add = (...numbers) => numbers.reduce((total, num) => total + num);
+
+const sub = (...numbers) => numbers.reduce((total, num) => total - num);
+
+const mul = (...numbers) => numbers.reduce((total, num) => total * num);
+
+const min = (...numbers) => {
+    let x = numbers[0];
+    numbers.forEach(number => {
+        if (x > number) x = number;
+    });
+    return x;
+}
+
+const max = (...numbers) => {
+    let x = numbers[0];
+    numbers.forEach(number => {
+        if (x < number) x = number;
+    });
+    return x;
+}
+
 module.exports = {
     identity,
     addb,
     subb,
     mulb,
     minb,
-    maxb
+    maxb,
+    add,
+    sub,
+    mul,
+    min,
+    max
 };

c6a8b71303d17d479c69759e5d88b020011c3353
hirafs <hiraa3708@gmail.com>
2020-09-04 10:46:53 -0700

    add-hira

---
+++ Solutions/hirasolutions.js
@@ -0,0 +1,26 @@
+// 1. Returns the argument
+const identity = (arg) => arg;
+identity('Hi, Im Hira');
+
+//Sum of two numbers
+
+const addNum = (one, two)
+=> one + two;
+addNum(one, two);
+
+addNum(9,8);
+
+//Takes two numbers and returns difference
+
+const subNum = (one, two)
+=> one - two;
+subNum(one, two);
+
+subNum(4,8); 
+
+//Takes two numbers and returns product
+const mulNum = (one, two)
+=> one * two;
+mulNum(one, two);
+
+mulNum(4,8); 

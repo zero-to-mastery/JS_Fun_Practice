db1d299e09e6a03dd5d8b236daafe083c542c791
kunal prakash <kunalprakash1309@gamil.com>
2020-09-04 12:20:59 +0530

    added solution from 5-10

--- Solutions/kunalprakash1309.js
+++ Solutions/kunalprakash1309.js
@@ -28,10 +28,56 @@ const minb = (a, b) => a < b ? a : b;
 minb(12, 10)                // 10
 
 
+// takes two numbers and returns the larger one
+const maxb = (a, b) => a > b ? a : b;
+
+maxb(20, 25)                // 25
+
+
+// add that is genralized for any amount of arguments
+const add = (...nums) => nums.reduce((acc, num) => {return acc + num}, 0);
+
+add(1, 2, 3)                // 6
+
+
+// sub that is generalized for any amount of arguments
+const sub = (...nums) => {
+    let result = nums[0];
+    for (let i = 1; i < nums.length; i++){
+        result = result - nums[i];
+    }
+    return result;
+}
+
+sub(100, 50, 25)             // 25
+
+
+// mul that is genralized for any amount of arguments
+const mul = (...nums) => nums.reduce((acc, num) => {return acc * num}, 1)
+
+mul(6, 5, 4, 3, 2, 1)        // 720
+
+
+// min that is genralized for any amount of arguments
+const min = (...nums) => {
+    let minimum = nums[0];
+    for(let i = 1; i < nums.length; i++){
+        minimum = minimum < nums[i] ? minimum : nums[i]; 
+    }
+    return minimum
+}
+
+min(30, 40, 55, 10)          // 10
+
 module.exports = {
     identity,
     addb,
     subbb,
     mulb,
     minb,
+    maxb,
+    add,
+    sub,
+    mul,
+    min,
 };

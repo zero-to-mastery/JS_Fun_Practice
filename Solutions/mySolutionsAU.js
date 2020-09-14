a9810972c4f97c82eb3bddbfb7a424988d0981d6
Anurag Upperwal <anuragupperwal@Anurags-MacBook-Pro.local>
2020-09-14 22:31:52 +0530

    added more functions

---
+++ Solutions/mySolutionsAU.js
@@ -0,0 +1,11 @@
+//to add two values
+const addb = (a,b) => a+b;
+console.log(addb(a,b));
+
+//to subtract two values
+const subb = (a,b) => a-b;
+console.log(subb(a,b));
+
+//to multiply two values
+const mulb = (a,b) => a*b;
+console.loglog(mulb(a,b));
--- test/mySolutionsAU.js
+++
@@ -1,3 +0,0 @@
-//to add two values
-const addb = (a,b) => a+b;
-console.log(addb(a,b));

90d2e61ecd6824e6006e322e4775889637d23232
Mariko971 <slimmack@gmail.com>
2021-02-21 15:25:48 +0400

    Added filter and filterTail functions

--- Solutions/mariko_971_solutions.js
+++ Solutions/mariko_971_solutions.js
@@ -216,6 +216,29 @@ const collect = (gen,array)=>{
   }
 }
 
+function filter(gen,pre){
+ function* gen2(){
+    let val = gen();
+    let pred = pre(val);
+    while(val !== undefined){
+        if(pred===true){
+        return val;
+        }
+      return gen2().next().value;
+    }
+  } 
+  return ()=>gen2().next().value
+}
+
+function filterTail(gen,pre){
+  let val = gen();
+  while(val !== undefined){
+    if(pre(val)===true){
+    return ()=> val;
+    }
+    return ()=>filterTail(gen,pre)
+    }
+}
 
 
   module.exports = { identity, addb, subb, mulb, minb, maxb, add, sub, mul, min, max, addRecurse, mulRecurse, minRecurse, maxRecurse, not, acc, accPartial, accRecurse, fill, fillRecurse, set, identityf, addf, liftf, pure, curryb, curry, inc, twiceUnary, doubl, square, twice, reverseb, reverse, composeuTwo, composeu, composeb, composeTwo, compose, limitb, limit, genFrom, genTo, genFromTo, elementGen, element, collect, };//, filter, filterTail, concatTwo, concat, concatTail, gensymf, gensymff, fibonaccif, counter, revocableb, revocable, extract, m, addmTwo, addm, liftmbM, liftmb, liftm, exp, expn, addg, liftg, arrayg, continuizeu, continuize, vector, exploitVector, vectorSafe, pubsub, mapRecurse, filterRecurse, }; 

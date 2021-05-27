8e5e1d7a5ff745dd23609827b4b0b60a70c176f4
Vincent <mcken.vince@gmail.com>
2021-05-26 20:49:55 -0600

    completed first four functions

---
+++ Solutions/js-fun-practice-vinavoovoo.js
@@ -0,0 +1,13 @@
+// ZTM JS_Fun_Practice
+
+// takes an argument and returns it
+const identity = (x) => x;
+
+// returns sum of two numbers
+const addb = (a, b) => a + b;
+
+// returns difference of two numbers
+const subb = (a, b) => a - b;
+
+// returns product of two numbers
+const mulb = (a, b) => a * b;

007ba6481a86db4f1126baf8f6e56c46a2fa484b
Vincent <mcken.vince@gmail.com>
2021-05-26 20:53:49 -0600

    added module.exports to bottom of my solution file

--- Solutions/js-fun-practice-vinavoovoo.js
+++ Solutions/js-fun-practice-vinavoovoo.js
@@ -11,3 +11,7 @@ const subb = (a, b) => a - b;
 
 // returns product of two numbers
 const mulb = (a, b) => a * b;
+
+
+
+module.exports = { identity, addb, subb, mulb, minb, maxb, add, sub, mul, min, max, addRecurse, mulRecurse, minRecurse, maxRecurse, not, acc, accPartial, accRecurse, fill, fillRecurse, set, identityf, addf, liftf, pure, curryb, curry, inc, twiceUnary, doubl, square, twice, reverseb, reverse, composeuTwo, composeu, composeb, composeTwo, compose, limitb, limit, genFrom, genTo, genFromTo, elementGen, element, collect, filter, filterTail, concatTwo, concat, concatTail, gensymf, gensymff, fibonaccif, counter, revocableb, revocable, extract, m, addmTwo, addm, liftmbM, liftmb, liftm, exp, expn, addg, liftg, arrayg, continuizeu, continuize, vector, exploitVector, vectorSafe, pubsub, mapRecurse, filterRecurse, };

eb9ba595ea5ecca310e593d31b1ea892497d807d
Burrito {} <william.hurley333@gmail.com>
2021-06-30 23:13:41 +1000

    adding a solution

---
+++ index.js
@@ -0,0 +1,16 @@
+
+array = [1, 2, 3];
+if (array){
+ for(array = array.length; array <= 10; array++){
+console.log(array)
+ }
+}
+
+ 
+op = [1, 2, 3] 
+num = " over here!";
+if (op === 1, 2, 3){
+    for (op = 0; op <= 100; op++){
+    console.log(op + num);
+} 
+}

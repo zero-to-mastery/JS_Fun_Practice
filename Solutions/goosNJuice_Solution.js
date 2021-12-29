284197bdfcbcb067ba319a0ad12e73aef37c3b10
gnj <zamirkononov@gmail.com>
2021-12-29 15:51:59 +0200

    addb func

--- Solutions/goosNJuice_Solution.js
+++ Solutions/goosNJuice_Solution.js
@@ -1,2 +1,6 @@
 // Write a function identity that takes an argument and returns that argument
 const identity = (argument) => argument;
+
+//Write a binary function addb that takes two numbers and returns their sum
+const addb = (a, b) => a + b;
+

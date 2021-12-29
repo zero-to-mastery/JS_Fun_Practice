5702c1fae562e6850264eb2dffb9fd194d93f045
gnj <zamirkononov@gmail.com>
2021-12-29 15:53:29 +0200

    optimised exports

--- Solutions/goosNJuice_Solution.js
+++ Solutions/goosNJuice_Solution.js
@@ -1,6 +1,6 @@
 // Write a function identity that takes an argument and returns that argument
-const identity = (argument) => argument;
+export const identity = (argument) => argument;
 
 //Write a binary function addb that takes two numbers and returns their sum
-const addb = (a, b) => a + b;
+export const addb = (a, b) => a + b;
 

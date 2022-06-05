5c8b3b1d266643c6681aae6d69df994d490fc97e
Tellula <julieloane@gmail.com>
2022-06-05 19:55:17 +0200

    solve until compose

--- Solutions/tellula_solutions.js
+++ Solutions/tellula_solutions.js
@@ -144,6 +144,176 @@ let fill = (num) => {
 
 //Write a function fillRecurse that does what fill does but uses recursion
 
+let fillRecurse = (num,array) => {
+  if (!array) {
+    array = []
+  }
+  if (array.length === num) {
+    return array
+  } else {
+    array.push(num);
+    return fillRecurse(num,array)
+  }
+}
+
+// Write a function set that is given a list of arguments and returns an array with all duplicates removed
+
+let set = (...nums) => {
+  let array = new Array(...nums)
+  for (let i = 0 ; i < array.length ; i++) {
+    if (array[i] === array[i+1]) {
+      array.splice(array[i], 2);
+    }
+  }
+  return array
+}
+
+// Write a function identityf that takes an argument and returns a function that returns that argument
+let identityf = (x) => {
+  return function() {
+    return x
+  }
+}
+
+//Write a function addf that adds from two invocations
+let addf = (a) => (b) => a + b
+
+
+// Write a function liftf that takes a binary function, and makes it callable with two invocations
+
+let liftf = (binaryFunc) => {
+return (a) => (b) => binaryFunc(a,b)
+}
+
+
+// Write a pure function pure that is a wrapper arround the impure function impure
+function impure(x) {
+  y++;
+  z = x * y;
+}
+
+var y = 5, z;
+
+
+// Returns: array - an array containing y and z
+let pure = (x, y) => {
+  let z = ++y * x
+  let newArr = [y, z];
+  return newArr
+}
+
+// Write a function curryb that takes a binary function and an argument, and returns a function that can take a second argument
+let curryb = (binaryFunc, a) => {
+return (b) => {
+  return binaryFunc(a,b)
+}
+}
+
+// Write a function curry that is generalized for any amount of arguments
+let curry = (func, ...outer) => {
+return (...nums) => {
+  return func(...outer, ...nums)
+}
+}
+
+// Without writting any new functions, show multiple ways to create the inc function
+let inc = (x) => ++x
+
+// Write a function twiceUnary that takes a binary function and returns a unary function that passes its argument to the binary function twice
+let twiceUnary = (binaryFunc) => {
+    return (a) => {
+      return binaryFunc(a, a)
+    }
+  }
+  
+// Use the function twiceUnary to create the doubl function
+let doubl = (x) => {
+  return twiceUnary(addb)(x);
+}
+
+// Use the function twiceUnary to create the square function
+let square = (x) => {
+  return twiceUnary(mulb)(x)
+}
+
+// Write a function twice that is generalized for any amount of arguments
+
+let twice = (x) => {
+  return (...args) => {
+    return x(...args,...args)
+  }
+}
+// Write a function reverseb that reverses the arguments of a binary function
+let reverseb = (binaryFunc) => {
+return (a,b) => {
+  return binaryFunc(b,a)
+}
+}
+
+// Write a function reverse that is generalized for any amount of arguments
+let reverse = (binaryFunc) => {
+return (...args) => {
+  return binaryFunc(...args.reverse())
+}
+}
+// Write a function composeuTwo that takes two unary functions and returns a unary function that calls them both
+let composeuTwo = (unaryFunc1, unaryFunc2) => {
+  return (x) => {
+    x = unaryFunc1(x);
+    return unaryFunc2(x);
+  };
+};
+
+// Write a function composeu that is generalized for any amount of arguments
+let composeu = (...funcs) => 
+  (x) => {
+    funcs.forEach((func) => (x = func(x)));
+    return x
+  };
+ 
+// Write a function composeb that takes two binary functions and returns a function that calls them both
+let composeb = (binaryFunc1, binaryFunc2) => (a, b, c) => {
+  return binaryFunc2(binaryFunc1(a, b), c);
+};
+
+// Write a function composeTwo that takes two functions and returns a function that calls them both
+let composeTwo = (binaryFunc1, binaryFunc2) =>
+(...nums) => {
+  nums = binaryFunc1(...nums)
+  return binaryFunc2(nums);
+}
+
+// Write a function compose that takes any amount of functions and returns a function that takes any amount of arguments and gives them to the first function, then that result to the second function and so on
+let compose =
+  (...funcs) =>
+  (...args) => {
+    let x = funcs[0](...args);
+    for (let i = 1; i < funcs.length; i++) {
+      if (typeof x === 'object') {
+        x = funcs[i](...(x.values()));
+        continue
+      } 
+        x = funcs[i](x);
+    }
+    return x;
+  };
+
+const f = compose(add, doubl, fill, max)
+console.log(f(0, 1, 2))
+
+// add(0, 1, 2) -> 3
+// doubl(3) -> 6
+// fill(6) -> [ 6, 6, 6, 6, 6, 6 ]
+// max([ 6, 6, 6, 6, 6, 6 ]) -> 6
+
+
+
+
+
+
+
+
+
 
 
 
@@ -168,26 +338,26 @@ module.exports = {
   accPartial,
   accRecurse,
   fill,
-  // fillRecurse,
-  // set,
-  // identityf,
-  // addf,
-  // liftf,
-  // pure,
-  // curryb,
-  // curry,
-  // inc,
-  // twiceUnary,
-  // doubl,
-  // square,
-  // twice,
-  // reverseb,
-  // reverse,
-  // composeuTwo,
-  // composeu,
-  // composeb,
-  // composeTwo,
-  // compose,
+  fillRecurse,
+  set,
+  identityf,
+  addf,
+  liftf,
+  pure,
+  curryb,
+  curry,
+  inc,
+  twiceUnary,
+  doubl,
+  square,
+  twice,
+  reverseb,
+  reverse,
+  composeuTwo,
+  composeu,
+  composeb,
+  composeTwo,
+  compose,
   // limitb,
   // limit,
   // genFrom,

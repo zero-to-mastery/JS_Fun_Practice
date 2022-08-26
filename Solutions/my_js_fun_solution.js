e14cad02accf03e2ff97ebdd168cc25de7317be5
Mrinal-xx-Singha <mrinalsingha17@gmail.com>
2022-08-26 18:13:11 +0530

    done some exercises

---
+++ Solutions/my_js_fun_solution.js
@@ -0,0 +1,37 @@
+// Write a function identity that takes an argument and returns that argument
+const identity = (x) => x;
+// Write a binary function addb that takes two numbers and returns their sum
+function addb (x,y){
+    return x + y;
+}
+// Write a binary function subb that takes two numbers and returns their difference
+const subb = (x,y) => x-y;
+
+// Write a binary function mulb that takes two numbers and returns their product
+const mulb=(a, b) => x * y;
+
+// Write a binary function minb that takes two numbers and returns the smaller one
+function minb (a,b) {
+    if(a<b){
+        return a;
+
+    }else{
+        return b;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { identity, addb, subb, mulb, minb, /*maxb, add, sub, mul, min, max, addRecurse, mulRecurse, minRecurse, maxRecurse, not, acc, accPartial, accRecurse, fill, fillRecurse, set, identityf, addf, liftf, pure, curryb, curry, inc, twiceUnary, doubl, square, twice, reverseb, reverse, composeuTwo, composeu, composeb, composeTwo, compose, limitb, limit, genFrom, genTo, genFromTo, elementGen, element, collect, filter, filterTail, concatTwo, concat, concatTail, gensymf, gensymff, fibonaccif, counter, revocableb, revocable, extract, m, addmTwo, addm, liftmbM, liftmb, liftm, exp, expn, addg, liftg, arrayg, continuizeu, continuize, vector, exploitVector, vectorSafe, pubsub, mapRecurse, filterRecurse,*/ };

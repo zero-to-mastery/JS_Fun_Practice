807550e05c9f2488e54fe57524677fada36427f4
Jeramiha Holt <jeramihaholt04@gmail.com>
2022-11-27 18:49:47 -0800

    adding changes I have made to Solutions file

---
+++ Solutions/ezClops_Solution.js
@@ -0,0 +1,105 @@
+const { identity } = require("lodash");
+
+/*Write a function identity that takes an argument and returns that argument*/
+function identity(x){
+    return x;
+}
+/*Write a binary function addb that takes two numbers and returns their sum*/
+function addb(a, b) {
+    return a + b;
+}
+/*Write a binary function subb that takes two numbers and returns their difference*/
+function subb(a, b) {
+    return b - a;
+}
+/*Write a binary function mulb that takes two numbers and returns their product*/
+function mulb(a, b) {
+    return a * b;
+}
+/*Write a binary function minb that takes two numbers and returns
+the smaller one*/
+function minb(a, b) {
+    if (a < b) return a;
+    return b;
+}
+/*Write a binary function maxb that takes two numbers and returns the larger one*/
+function maxb(a, b) {
+    if (a > b) return a;
+    return b;
+}
+/*Write a function add that is generalized for any amount of arguments*/
+function add(...num) {
+    let array = [...num];
+    let array2 = array[0];
+    let sum = 0;
+    array2.forEach(n => sum += n);
+    return sum;
+}
+/*Write a function sub that is generalized for any amount of arguments*/
+function sub(...num) {
+    let array = [...num];
+    let array2 = array[0];
+    let subt = 0;
+    array2.forEach((n, i) => {
+        if(i === 0){subt = n;}
+        else{subt -= n;}
+    });
+    return subt;
+}
+/*Write a function mul that is generalized for any amount of arguments*/
+function mul(...num) {
+    let array = [...num];
+    let array2 = array[0];
+    let mult = 1;
+    array2.forEach(n => mult *= n);
+    return mult;    
+}
+/*Write a function min that is generalized for any amount of arguments*/
+function min(...num) {
+    let array = [...num];
+    let array2 = array[0];
+    let mini = 0;
+    array2.forEach((n, i) => {
+        if (i === 0){mini = n;}
+        for (j = i + 1; j < array2.length; j++) {
+            if (array2[j] < mini){mini = array2[j]}    
+        }
+    });
+    return mini;
+}
+/*Write a function max that is generalized for any amount of arguments*/
+function max(...num) {
+    let array = [...num];
+    let array2 = array[0];
+    let maxi = 0;
+    array2.forEach((n, i) => {
+        if (i === 0){maxi = n;}
+        for (j = i + 1; j < array2.length; j++) {
+            if (array2[j] > maxi){maxi = array2[j]}    
+        }
+    });
+    return maxi;    
+}
+/*Write a function addRecurse that is the generalized add function but uses recursion*/
+function addRecurse(...num) {
+    const array = [...num];
+    const array2 = array[0];
+    let sum = 0;
+    let value = array2.shift();
+    
+    return sum;
+}
+
+
+module.exports = {
+    identity,
+    addb,
+    subb,
+    mulb,
+    minb,
+    add,
+    sub,
+    mul,
+    min,
+    max
+};

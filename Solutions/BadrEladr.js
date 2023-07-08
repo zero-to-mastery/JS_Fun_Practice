9e962124be7ad6250c4bb2a3cb9317c7da7c8fb5
badrEladraoui1 <eladraoui.badr@gmail.com>
2023-07-08 20:30:43 +0100

    first function

--- Solutions/BadrEladr.js
+++ Solutions/BadrEladr.js
@@ -1,1 +1,13 @@
-// just startings
+// just starting
+
+//link
+//https://github.com/zero-to-mastery/JS_Fun_Practice
+
+// identity(x) ⇒ any
+// Write a function identity that takes an argument and returns that argument
+
+const getArg = (arg => arg);
+
+let argm = 2; 
+getArg(argm);
+

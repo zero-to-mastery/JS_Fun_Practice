2fb57471422edffa96ee5dbcb5484b3a364d4e68
Vusal <vusal.novruzov.1@iliauni.edu.ge>
2024-01-03 19:02:59 +0400

    made first contribution

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-
 array = [1, 2, 3];
-if (array){
- for(array = array.length; array <= 10; array++){
-console.log(array)
- }
+if (array) {
+  for (array = array.length; array <= 10; array++) {
+    console.log(array);
+  }
 }
 
- 
-op = [1, 2, 3] 
+op = [1, 2, 3];
 num = " over here!";
-if (op === 1, 2, 3){
-    for (op = 0; op <= 100; op++){
+if ((op === 1, 2, 3)) {
+  for (op = 0; op <= 100; op++) {
     console.log(op + num);
-} 
+  }
 }
 
+// array methods
 
+const newArray = [1, 2, 3, 4];
+
+const mutatedArray = newArray.map((num) => num * 2);
+
+console.log(mutatedArray);
